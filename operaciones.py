aaac5ff0dfd1dbf05cb1f3c9f38d04b6627a145c
csantv <csantve@gmail.com>
2020-05-05 19:11:17 -0500

    fix num_nodos, num_hojas y get_valores

--- operaciones.py
+++ operaciones.py
@@ -1,30 +1,43 @@
 from typing import List
 
-from arbol import Arbol, _NodoArbol
+from arbol import _NodoArbol
 from bosque import Bosque
-from recorridos import preorden, postorden, niveles, frontera
 
 
-def num_hojas(a: Arbol) -> int:
+def num_hojas(b: Bosque) -> int:
     num = 0
-    nodes = preorden(a)
-    for node in nodes:
+    node = b.nodo
+    while node is not None:
         if len(node.primog) == 0:
             num += 1
+        else:
+            num += num_hojas(node.primog)
+        node = node.sig_herm
     return num
 
 
-def grado_arbol(a: Arbol) -> int:
+def grado_bosque(b: Bosque) -> int:
     max_grado = 0
-    nodes = preorden(a)
-    for node in nodes:
-        if len(node.primog) > max_grado:
-            max_grado = len(node.primog)
+    node = b.nodo
+    while node is not None:
+        if len(b) != 0:
+            size = len(b)
+            max_grado = max(max_grado, size)
+        grado_bosque(node.primog)
+        node = node.sig_herm
     return max_grado
 
 
-def num_nodos(a: Arbol) -> int:
-    return len(preorden(a))
+def num_nodos(b: Bosque) -> int:
+    # se incluye el nodo raiz
+    total = 1
+    node = b.nodo
+    while node is not None:
+        if len(node.primog) != 0:
+            total += num_nodos(node.primog) - 1
+        total += 1
+        node = node.sig_herm
+    return total
 
 
 def altura(b: Bosque) -> int:
@@ -36,4 +49,6 @@ def altura(b: Bosque) -> int:
 
 
 def get_valores(lst: List[_NodoArbol]) -> None:
-    print([node.valor for node in lst])
+    new_lst = [node.valor for node in lst]
+    new_lst.reverse()
+    print(new_lst)
